6160941aca15e21aca42d28e1852046c5b5c8a0e
luizzz16 <lmedina9@ucol.mx>
2026-02-20 08:39:03 -0600

    creacion de objeto contacto

--- js/Agenda/js/agenda.js
+++ js/Agenda/js/agenda.js
@@ -4,6 +4,15 @@ let contactos = [];
 
 let seguir = true;
 
+let contacto = {
+    id: 0,
+    nombre: '',
+    apellidos: '',
+    facultad: '',
+    telefono: '',
+    edad: 0
+}
+
 function mostrarContactos(cotactos) {
     for (let contacto of contactos) {
                     mensaje += `Nombre: ${contacto.nombre} ${contacto.apellidos} Facultad: ${contacto.facultad} Telefono: ${contacto.telefono}Edad: ${contacto.edad}`;
@@ -16,19 +25,19 @@ while (seguir) {
 
     opcion = parseInt(opcion);
 
-    let nombre, apellidos, facultad, telefono, edad;
 
     switch (opcion) {
         case 1:
             alert("Agregar contacto");
 
-            nombre = prompt("Ingrese el nombre del contacto");
-            apellidos = prompt("Ingresa los apellidos");
-            facultad = prompt("Ingresa la facultad");
-            telefono = prompt("Ingresa el telefono");
-            edad = prompt("Ingresa la edad");
+            contacto.id = contactos.length + 1;
+            contacto.nombre = prompt("Ingrese el nombre del contacto");
+            contacto.apellidos = prompt("Ingresa los apellidos");
+            contacto.facultad = prompt("Ingresa la facultad");
+            contacto.telefono = prompt("Ingresa el telefono");
+            contacto.edad = parseInt(prompt("Ingresa la edad"));
 
-            contactos.push({ nombre, apellidos, facultad, telefono, edad });
+            contactos.push(contacto);
             console.log(contactos);
 
             alert("Contacto agregado correctamente");
@@ -43,7 +52,7 @@ while (seguir) {
                 let mensaje = "";
 
                 for (let contacto of contactos) {
-                    mensaje += `Nombre: ${contacto.nombre} \n Apellidos: ${contacto.apellidos} \n Facultad: ${contacto.facultad} \n Telefono: ${contacto.telefono} \n Edad: ${contacto.edad}\n`;
+                    mensaje += `ID: ${contacto.id} \n Nombre: ${contacto.nombre} \n Apellidos: ${contacto.apellidos} \n Facultad: ${contacto.facultad} \n Telefono: ${contacto.telefono} \n Edad: ${contacto.edad}\n`;
                 }
                 alert(mensaje);
             }
@@ -51,27 +60,22 @@ while (seguir) {
 
         case 3:
             alert("Actualizar contactos");
-            let contactoActualizar = prompt("Ingrese el nombre del contacto a actualizar");
+            let contactoActualizar = prompt("Ingrase el id del usuario a actualizar");
             for (let contacto of contactos) {
-                if (contacto.nombre === contactoActualizar) {
-                    nombre = prompt("Ingrese el nuevo nombre del contacto");
-                    apellidos = prompt("Ingresa los nuevos apellidos");
-                    facultad = prompt("Ingresa la nueva facultad");
-                    telefono = prompt("Ingresa el nuevo telefono");
-                    edad = prompt("Ingresa la nueva edad");
-                    contacto.nombre = nombre;
-                    contacto.apellidos = apellidos;
-                    contacto.facultad = facultad;
-                    contacto.telefono = telefono;
-                    contacto.edad = edad;
+                if (contacto.id === parseInt(contactoActualizar)) {
+                    contacto.nombre = prompt("Ingrese el nuevo nombre del contacto");
+                    contacto.apellidos = prompt("Ingresa los nuevos apellidos");
+                    contacto.facultad = prompt("Ingresa la nueva facultad");
+                    contacto.telefono = prompt("Ingresa el nuevo telefono");
+                    contacto.edad = parseInt(prompt("Ingresa la nueva edad"));
             }
         }
             break;
         case 4:
             alert("Eliminar contacto");
-            let contactoEliminar = prompt ("Ingrese el nombre del contacto a eliminar");
+            let contactoEliminar = prompt ("Ingrese el id del contacto a eliminar");
             for (let contacto of contactos) {
-                if (contacto.nombre === contactoEliminar) {
+                if (contacto.id === parseInt(contactoEliminar)) {
                     let index = contactos.indexOf(contacto);
                     contactos.splice(index, 1);
                 alert("Contacto eliminado correctamente");
